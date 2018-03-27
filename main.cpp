d614aa1e80ec71c457ffb9fba35b793b1bd8deb7
Nelinaivanova <n_ivanow@abv.bg>
2018-03-27 19:40:01 +0000

    Detailed err on overflow

--- main.cpp
+++ main.cpp
@@ -81,14 +81,14 @@ int main()
             // "1 2" => "2"
             if (*arguments < '0' || *arguments > '9' || arguments[1] != '\0') {
                 // invalid register - must be just one digit at end of string
-                cout << "ERROR" << endl;
+                cout << "ERROR:overflow" << endl;
                 continue; // next loop of the repl
             }
             int reg3 = *arguments - '0';
             
             int sum = registers[reg2] + registers[reg3];
             if (sum > 100 || sum < -100) {
-                cout << "ERROR" << endl;
+                cout << "ERROR:" << endl;
                 continue; // next loop of the repl
             }
             
